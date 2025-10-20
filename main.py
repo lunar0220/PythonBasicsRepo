bd660f1377bce67293a1849f4ef419f26d21bdfc
Анастасия Власова <anastasiavlasova@MacBook-Air-Anastasia-2.local>
2025-10-20 16:17:01 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+num_1: int = int(input("Enter first number: "))
+num_2: int = int(input("Enter second number: "))
+
+
+print(num_1, "+", num_2, "=", num_1+num_2)
+print(num_1, "-", num_2, "=", num_1-num_2)
+print(num_1, "*", num_2, "=", num_1*num_2)
